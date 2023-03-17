c5fb5f6e30692432cf0bd6989180367c67e54466
joshuaali8 <alijoshua12@hotmail.com>
2023-03-17 22:22:42 +0000

    cetladmin

--- App/models/CETLAdmin.py
+++ App/models/CETLAdmin.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from App.database import db
 
+
+#make user class abstract and reference into cetladmin
 class CETLAdmin (db.Model):
     CETLAdminID = db.Column(db.Integer, primary_key=True)
 
